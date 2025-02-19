e93e99adfa6b59432c89cde2924103cbbb6fd616
Roy Ernst <itsrovey@icloud.com>
2025-02-19 21:47:43 +0100

    [FIX] Set completed_at date to created_at date when is_completed is true when migrating

--- database/migrations/2025_02_19_193136_add_completed_at_to_todos_table.php
+++ database/migrations/2025_02_19_193136_add_completed_at_to_todos_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -11,6 +12,12 @@ return new class extends Migration
         Schema::table('todos', function (Blueprint $table) {
             $table->date('completed_at')->nullable()->after('priority');
         });
+
+        DB::table('todos')
+            ->where('is_completed', true)
+            ->update([
+                'completed_at' => DB::raw('DATE(created_at)'),
+            ]);
     }
 
     public function down(): void
