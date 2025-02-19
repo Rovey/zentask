b04d11559c065158856a73e576b1063d432619ca
Roy Ernst <itsrovey@icloud.com>
2025-02-19 20:54:17 +0100

    [FIX] Add user to newly created team so it shows up in tenant selector

--- app/Filament/Resources/TeamResource/Pages/CreateTeam.php
+++ app/Filament/Resources/TeamResource/Pages/CreateTeam.php
@@ -3,10 +3,15 @@
 namespace App\Filament\Resources\TeamResource\Pages;
 
 use App\Filament\Resources\TeamResource;
-use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Support\Facades\Auth;
 
 class CreateTeam extends CreateRecord
 {
     protected static string $resource = TeamResource::class;
+
+    public function afterCreate()
+    {
+        $this->record->users()->attach(Auth::id());
+    }
 }
