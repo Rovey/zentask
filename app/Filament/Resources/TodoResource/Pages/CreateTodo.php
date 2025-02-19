dfd2b00a8f76a46e11573d0299203592db4de837
Roy Ernst <itsrovey@icloud.com>
2025-02-19 20:57:29 +0100

    [FIX] Redirect to index after creating or editing a todo

--- app/Filament/Resources/TodoResource/Pages/CreateTodo.php
+++ app/Filament/Resources/TodoResource/Pages/CreateTodo.php
@@ -3,10 +3,14 @@
 namespace App\Filament\Resources\TodoResource\Pages;
 
 use App\Filament\Resources\TodoResource;
-use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateTodo extends CreateRecord
 {
     protected static string $resource = TodoResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl();
+    }
 }
--- app/Filament/Resources/TodoResource/Pages/EditTodo.php
+++ app/Filament/Resources/TodoResource/Pages/EditTodo.php
@@ -10,6 +10,11 @@ class EditTodo extends EditRecord
 {
     protected static string $resource = TodoResource::class;
 
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl();
+    }
+
     protected function getHeaderActions(): array
     {
         return [
